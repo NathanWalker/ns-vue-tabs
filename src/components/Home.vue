8eead1b5b337dae4bd5a9ea61b3bdbf4e59b52b3
Nathan Walker <walkerrunpdx@gmail.com>
2025-12-14 17:39:23 -0800

    feat: advanced hmr approaches
    cleanup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,22 @@
 <script lang="ts" setup>
-import { customizeTabs } from "./tab-customize";
+import { EventData, type TabView } from "@nativescript/core";
+import { tabCustomizer } from "./tab-customize";
 
 const items = [];
 for (let i = 0; i < 100; i++) {
   items.push({
     title: `Item ${i + 1}`,
   });
+}
+function customize(args: EventData) {
+  tabCustomizer.customizeTabs(args);
+}
+function scrollChange(args: EventData) {
+  tabCustomizer.scrollChange(args);
+}
+function tapHome() {
+  // Reset accessory on tap
+  tabCustomizer.scrollChange({ value: 0 } as any);
 }
 </script>
 
@@ -16,14 +27,16 @@ for (let i = 0; i < 100; i++) {
         iosTabBarMinimizeBehavior="onScrollDown"
         tabTextFontSize="11"
         selectedTabTextColor="#fff"
-        @loaded="customizeTabs"
+        @loaded="customize"
+        @tap="tapHome"
         class="bg-black"
       >
-        <TabViewItem title="Home" iconSource="sys://house.fill">
+        <TabViewItem title="Home" iconSource="sys://house.fill" >
           <ListView
             :items="items"
             separatorColor="transparent"
             class="bg-black"
+            @scrollChange="scrollChange"
           >
             <template #default="{ item }">
               <GridLayout class="p-4 m-2 bg-white/20 rounded-lg h-12">
